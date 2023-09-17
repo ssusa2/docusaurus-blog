4be00e0a8c296b64322f653935e5191156039303
jungsuin <audgns9207@naver.com>
2023-09-18 00:49:36 +0900

    blog status ready component

--- src/theme/BlogPostItem/Content/index.tsx
+++ src/theme/BlogPostItem/Content/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import clsx from 'clsx';
-import {blogPostContainerID} from '@docusaurus/utils-common';
-import {useBlogPost} from '@docusaurus/theme-common/internal';
-import MDXContent from '@theme/MDXContent';
-import type {Props} from '@theme/BlogPostItem/Content';
+import React from "react"
+import clsx from "clsx"
+import { blogPostContainerID } from "@docusaurus/utils-common"
+import { useBlogPost } from "@docusaurus/theme-common/internal"
+import MDXContent from "@theme/MDXContent"
+import type { Props } from "@theme/BlogPostItem/Content"
 
 export default function BlogPostItemContent({
   children,
   className,
 }: Props): JSX.Element {
-  const {isBlogPostPage} = useBlogPost();
+  const { isBlogPostPage } = useBlogPost()
+
+  const isReady =
+    children?.type.frontMatter.tags.at(-1) == "Ready" ? true : false
+  console.log("isReady", isReady)
   return (
     <div
       // This ID is used for the feed generation to locate the main content
       id={isBlogPostPage ? blogPostContainerID : undefined}
-      className={clsx('markdown', className)}
-      itemProp="articleBody">
+      className={clsx("markdown", className)}
+      itemProp='articleBody'
+    >
       <MDXContent>{children}</MDXContent>
+      {isReady ? (
+        <div>
+          <hr />
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <h2>글을 작성하고 있어요.</h2>
+            <img src='/img/ready.png' alt='준비중' />
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
-  );
+  )
 }
