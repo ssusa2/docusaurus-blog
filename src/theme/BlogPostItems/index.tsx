67b02737041c554cb136ae6768cf4d42d6c29fa0
jungsuin <audgns9207@naver.com>
2023-10-02 16:05:01 +0900

    flex test2

--- src/theme/BlogPostItems/index.tsx
+++ src/theme/BlogPostItems/index.tsx
@@ -28,52 +28,52 @@ export default function BlogPostItems({
           key={BlogPostContent.metadata.permalink}
           content={BlogPostContent}
         >
-          {/* <article> */}
-          <a
-            className='blog-list-item'
-            href={`/${BlogPostContent.frontMatter.slug}`}
-          >
-            <div className='blog-list-img-wrap'>
-              <img
-                className='blog-list-img'
-                src={`${BlogPostContent.frontMatter.image}`}
-              />
-            </div>
-            <div className='blog-list-content'>
-              <h3 className='blog-list-content-title'>
-                {BlogPostContent.frontMatter.title}
-              </h3>
-              <p className='blog-list-content-desc'>
-                {BlogPostContent.metadata.description}
-              </p>
-              <div>
-                <div className='blog-list-tag-list'>
+          <article>
+            <div
+              className='blog-list-item'
+              // href={`/${BlogPostContent.frontMatter.slug}`}
+            >
+              <div className='blog-list-img-wrap'>
+                <img
+                  className='blog-list-img'
+                  src={`${BlogPostContent.frontMatter.image}`}
+                />
+              </div>
+              <div className='blog-list-content'>
+                <h3 className='blog-list-content-title'>
+                  {BlogPostContent.frontMatter.title}
+                </h3>
+                <p className='blog-list-content-desc'>
+                  {BlogPostContent.metadata.description}
+                </p>
+                <div>
                   <div className='blog-list-tag-list'>
-                    {BlogPostContent.frontMatter.tags.map((el, i) => {
-                      // console.log("tag", el)
-                      return (
-                        <a
-                          className='blog-list-tag-item'
-                          key={i}
-                          href={`/tags/${el}`}
-                        >
-                          {`${el} `}
-                        </a>
-                      )
-                    })}
-                  </div>
+                    <div className='blog-list-tag-list'>
+                      {BlogPostContent.frontMatter.tags.map((el, i) => {
+                        console.log("tag", el)
+                        return (
+                          <a
+                            className='blog-list-tag-item'
+                            key={i}
+                            href={`/tags/${el}`}
+                          >
+                            {`${el} `}
+                          </a>
+                        )
+                      })}
+                    </div>
 
-                  <div className='blog-list-date'>
-                    <Date
-                      date={BlogPostContent.metadata.date}
-                      formattedDate={BlogPostContent.metadata.formattedDate}
-                    />
+                    <div className='blog-list-date'>
+                      <Date
+                        date={BlogPostContent.metadata.date}
+                        formattedDate={BlogPostContent.metadata.formattedDate}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </a>
-          {/* </article> */}
+          </article>
         </BlogPostProvider>
       ))}
     </>
