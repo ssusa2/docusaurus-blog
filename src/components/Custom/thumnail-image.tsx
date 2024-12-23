d42efafb6958699452793e378800a8f0ff036f2f
jungsuin <audgns9207@naver.com>
2024-12-24 01:59:20 +0900

    Modify: Post Thumnail-image optimize

--- src/components/Custom/thumnail-image.tsx
+++ src/components/Custom/thumnail-image.tsx
@@ -4,7 +4,14 @@ export const ThumNailImage = ({ src, alt }) => {
   const [loaded, setLoaded] = React.useState(false)
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '334px' }}>
+    <div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '334px',
+        marginBottom: '20px',
+      }}
+    >
       {!loaded && (
         <div
           style={{
@@ -17,13 +24,16 @@ export const ThumNailImage = ({ src, alt }) => {
       <img
         style={{
           width: '100%',
+          height: '100%',
           objectFit: 'cover',
-          display: loaded ? 'block' : 'none',
+          opacity: loaded ? 1 : 0, // 로드 전에는 투명
+          transition: 'opacity 0.5s ease-in-out', // 서서히 나타나는 효과
+          display: 'block',
         }}
-        alt={`${alt}`}
-        src={`${src}`}
-        onLoad={() => setLoaded(true)}
-        loading='lazy'
+        alt={alt}
+        src={src}
+        onLoad={() => setLoaded(true)} // 로드 후 상태 변경
+        loading='eager'
       />
     </div>
   )
